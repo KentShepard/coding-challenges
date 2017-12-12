5a5caeaf4fbd62f5b79efbe5b8a47ab10079aa38
KentShepard <kentshepard7@gmail.com>
2017-12-12 14:24:44 -0500

    solve findDuplicates

---
+++ findDuplicates.js
@@ -0,0 +1,38 @@
+// Given two arrays of integers, find the duplicate elements between the two
+
+var findDuplicates = function (array1, array2) {
+  let results = [];
+  let obj = {};
+
+  array1.forEach(value => {
+    obj[value] = ++obj[value] || 1;
+  });
+
+  array2.forEach(value => {
+    if (obj[value] > 0) {
+      results.push(value);
+      obj[value]--;
+    }
+  })
+  return results;
+}
+
+// If input arrays are sorted
+var findDuplicates = function (array1, array2) {
+  let x = 0;
+  let y = 0;
+  let results = [];
+
+  while (x < array1.length && y < array2.length) {
+    if (array1[x] === array2[y]) {
+      results.push(array1[x])
+      x++;
+      y++;
+    } else if (array1[x] > array2[y]) {
+      y++;
+    } else if (array1[x] < array2[y]) {
+      x++;
+    }
+  }
+  return results;
+}
